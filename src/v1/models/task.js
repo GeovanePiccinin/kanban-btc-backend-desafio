e2475436fa35ee6f3f20d94b8072af8e17230f6e
Geovane <geovane.piccinin@gmail.com>
2024-12-18 19:48:15 -0300

    adding category attribute in task model

--- src/v1/models/task.js
+++ src/v1/models/task.js
@@ -1,24 +1,31 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const { schemaOptions } = require('./modelOptions')
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { schemaOptions } = require("./modelOptions");
 
-const taskSchema = new Schema({
-  section: {
-    type: Schema.Types.ObjectId,
-    ref: 'Section',
-    required: true
+const taskSchema = new Schema(
+  {
+    section: {
+      type: Schema.Types.ObjectId,
+      ref: "Section",
+      required: true,
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: String,
+      default: "",
+    },
+    position: {
+      type: Number,
+    },
   },
-  title: {
-    type: String,
-    default: ''
-  },
-  content: {
-    type: String,
-    default: ''
-  },
-  position: {
-    type: Number
-  }
-}, schemaOptions)
+  schemaOptions
+);
 
-module.exports = mongoose.model('Task', taskSchema)
+module.exports = mongoose.model("Task", taskSchema);
